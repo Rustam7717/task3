6cb93de472ce4c8a7ead5e23ba698c8f31d72443
Rustam7717 <dj_rustam85@mail.ru>
2022-09-07 14:09:48 +0600

    task3

---
+++ svetofor.js
@@ -0,0 +1,23 @@
+let svetofor = prompt('input color');
+switch(svetofor) {
+    case 'green':
+        alert ('Go!');
+        break;
+        case 'Green':
+            alert ('Go!');
+            break;
+    case 'yellow':
+        alert ('Warning');
+        break;
+        case 'Yellow':
+        alert('Warning!')
+        break;
+    case 'red':
+        alert('Stop!')
+        break;
+        case 'Red':
+            alert('Stop!!')
+        break;
+        default:
+        alert ('Undefined!');
+}
